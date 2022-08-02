e98354168aca64a93debe7c8a7e71e172f074d38
BoomerangRosalina <103407876+BoomerangRosalina@users.noreply.github.com>
2022-08-02 10:31:45 -0700

    Discord Sings Bot Version 1
    The first version of my Discord Sings bot that I have made and used before.

---
+++ README.txt
@@ -0,0 +1,59 @@
+DISCORD SINGS BOT.PY
+
+Ever wanted to do Discord sings events but you are kind of lonely or want a bot to join in? Well this source code is PERFECT for you.
+
+This source code will watch for a list of words or phrases and response with Lyrics that you set in it in the channels
+that are enabled to sing in.
+
+================================================
+
+Instructions for Setup
+1. Download/Fork this Source code
+2. Create a Bot Application in the Developer Portal and create a bot and copy the bot token
+3. At the bottom with client.run("INSERTBOTTOKENHERE"), replace INSERTBOTTOKENHERE with your bot token.
+4. On the On Message
+>>> responses = [], Add quotations and put in a lyric sample of what a response you want the bot to respond too. (See the example in main.py)
+5. Open singtrigger.txt and make a list of words or phrases you want the bot to watch for and respond with Lyrics you inputted
+6. Save Everything and run the bot.
+
+Optional Steps:
+At client = commands.Bot(command_prefix = '!'), replace ! with the prefix you want.
+
+=================================================
+
+HOW TO USE/HOW DOES IT WORK.
+
+To have a bot sing in a text channel you want, simply do <prefix>singtrue in the channel you want and the bot will store your channel id
+and know to sing in that channel if a set word or phrase(s) are being said in that channel.
+
+To disable the bot from singing in the enabled text channel, use <prefix>singfalse
+
+=================================================
+
+LICENSE:
+You are free to use, distribute, modify this source code however you please under the MIT license.
+
+============================================
+
+DISCLAIMER:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+USE OF THIS SOFTWARE IS AT THE RISK OF THE USER OBTAINING THIS SOFTWARE
+
+
+===========================================
+
+HELP
+If you need help, you can reach us out at Boomerang Rosalina's Development Discord Server: https://discord.gg/cbqDfn8jvd
+
+FOLLOW US:
+Website: https://boomerangrosalina.glitch.me/
+Youtube: https://www.youtube.com/channel/UCLEGbDdFIcw1k2KTZJxJzEQ
+Twitter: https://twitter.com/DevRosalina
+Subreddit: https://www.reddit.com/r/BoomRosalinaDev
---
+++ main.py
@@ -0,0 +1,109 @@
+import discord
+from discord.ext import commands
+import random
+import json
+import asyncio
+import sys
+
+client = commands.Bot(command_prefix = '!')
+client.remove_command("help")
+
+@client.event
+async def on_ready():
+    data = read_json("sings")
+    client.sings_channel = data["SingsChannels"]
+    await client.change_presence(status=discord.Status.idle)
+    print("Bot Ready")
+
+@client.event
+async def on_message(message):
+    member = message.author
+    if message.author.bot:
+        return
+    if client.user.id != message.author.id:
+        if message.content in singtrigger:
+            if message.channel.id in client.sings_channel:
+                async with message.channel.typing():
+                    await asyncio.sleep(2)
+                responses = ["Lyric Response 1",
+                             "Lyric Response 2",]
+                await message.channel.send(f'{random.choice(responses)}')
+            else:
+                return
+
+    await client.process_commands(message)
+
+
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(title="BOT COMMANDS", description="Here is my Commands", color=(2621184))
+    embed.add_field(name="STANDARD COMMANDS", value=">help - This Message\n>ping - Checks my Ping\n>say - Make me say things\n>esay - Make me say things in a embed", inline=False)
+    embed.add_field(name="CONFIG", value=">singtrue - Have me sing christmas songs randomly in your current channel\n>singfalse - Make me no longer sing in your text channel", inline=False)
+    embed.add_field(name="SOURCE CODE", value="https://github.com/BoomerangRosalina/DiscordSingsBot.py", inline=False)
+    await ctx.send(embed=embed)
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! My Latency is {round(client.latency * 1000)}ms')
+
+@client.command()
+async def say(ctx, *, question: commands.clean_content):
+    await ctx.send(f'{question}')
+    await ctx.message.delete()
+
+@client.command()
+async def esay(ctx, *, question):
+    embed = discord.Embed(description=f'{question}', color=(2621184))
+    await ctx.send(embed=embed)
+    await ctx.message.delete()
+
+@client.command()
+@commands.has_permissions(manage_guild=True)
+async def singtrue(ctx):
+    client.sings_channel.append(ctx.channel.id)
+    data = read_json("sings")
+    data["SingsChannels"].append(ctx.channel.id)
+    write_json(data, "sings")
+    await ctx.send("I will now randomly sing in this channel")
+
+@singtrue.error
+async def singtrue_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send('YOU REQUIRE THE MANAGE_GUILD PERMISSION TO USE THIS COMMAND')
+    else:
+        raise error
+
+@client.command()
+@commands.has_permissions(manage_guild=True)
+async def singfalse(ctx):
+    client.sings_channel.remove(ctx.channel.id)
+    data = read_json("sings")
+    data["SingsChannels"].remove(ctx.channel.id)
+    write_json(data, "sings")
+    await ctx.send("I will no longer randomly sing in this channel")
+
+@singfalse.error
+async def singfalse_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send('YOU REQUIRE THE MANAGE_GUILD PERMISSION TO USE THIS COMMAND')
+    else:
+        raise error
+
+def read_json(filename):
+    with open(f"./sings.json", "r") as file:
+        data = json.load(file)
+    return data
+
+def write_json(data, filename):
+    with open(f"./sings.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+if "__main__" == __name__:
+    with open("singtrigger.txt", "r") as f:
+        singtrigger = f.read().splitlines()
+
+
+
+
+
+client.run("INSERTBOTTOKENHERE")
